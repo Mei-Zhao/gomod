ea874b0c64ba98c72d674d7b6b0abf1ebabbd133
zhaomei <zhaomei@zhaomeis-MacBook-Pro.local>
2020-07-25 16:26:21 +0800

    add dependice use go get and module

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Mei-Zhao/gomod
 
 go 1.13
 
-require rsc.io/quote v1.5.2 // indirect
+require (
+	github.com/cheggaaa/pb/v3 v3.0.4 // indirect
+	rsc.io/quote v1.5.2 // indirect
+)
--- main.go
+++ main.go
@@ -1,11 +1,29 @@
 package main
 
 import (
-	"fmt"
+    "crypto/rand"
+    "fmt"
+    "io"
+    "io/ioutil"
 
-	"rsc.io/quote"
+    "github.com/cheggaaa/pb/v3"
+    "rsc.io/quote"
 )
 
 func main() {
-	fmt.Println(quote.Hello())
+    fmt.Println(quote.Hello())
+    var limit int64 = 1024 * 1024 * 500
+    // we will copy 200 Mb from /dev/rand to /dev/null
+    reader := io.LimitReader(rand.Reader, limit)
+    writer := ioutil.Discard
+
+    // start new bar
+    bar := pb.Full.Start64(limit)
+    // create proxy reader
+    barReader := bar.NewProxyReader(reader)
+    // copy from proxy reader
+    io.Copy(writer, barReader)
+    // finish bar
+    bar.Finish()
+
 }
